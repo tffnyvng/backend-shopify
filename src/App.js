ef0c30f6e67090315591757fd9662e84e5992875
Tiffany <tiffanyv1998@gmail.com>
2022-05-16 16:35:46 -0700

    start, handleChange, handleCreate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [form, setForm] = useState({
+    name: "",
+    location: "N/A",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+    console.log("form", form);
+  }
+
+  async function handleCreate(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:4000/items", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      const newItem = await response.json();
+      return newItem;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    async function getItems() {
+      try {
+        const response = await fetch("http://localhost:4000/items", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const items = await response.json();
+        setItems(items);
+        console.log(items);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <label>New item:</label>
+        <input name="name" value={form.name} onChange={handleChange}></input>
+        <label>Location:</label>
+        <input
+          name="location"
+          value={form.location}
+          onChange={handleChange}
+        ></input>
+        <button id="create" onClick={handleCreate}>
+          Add new item
+        </button>
+        <section>
+          <label>Items in inventory:</label>
+          {items &&
+            items.map((item) => {
+              const { name, id } = item;
+
+              return (
+                <ul key={id}>
+                  <li>
+                    {name}
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </li>
+                </ul>
+              );
+            })}
+        </section>
+      </form>
+      {/* <div>Hello</div> */}
+    </section>
   );
 }
 
