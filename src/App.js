5f0765b6cfc324046f194bbd96e8977b0501dc88
Tiffany <tiffanyv1998@gmail.com>
2022-05-16 20:12:23 -0700

    delete complete and edit wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import "./App.css";
+import "./App.css";
 import React, { useState, useEffect } from "react";
 
 function App() {
@@ -8,11 +8,13 @@ function App() {
     location: "N/A",
   });
 
+  //Storing the inputs into state
   function handleChange(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
     console.log("form", form);
   }
 
+  //Adding a new item into inventory
   async function handleCreate(e) {
     e.preventDefault();
 
@@ -26,12 +28,54 @@ function App() {
       });
 
       const newItem = await response.json();
+      setItems([...items, newItem]);
       return newItem;
     } catch (err) {
       console.error(err);
     }
   }
 
+  //Editing the item
+
+  const editpage = document.getElementById("editpage");
+  const overlay = document.getElementById("overlay");
+
+  async function openEditPage(e) {
+    e.preventDefault();
+
+    if (editpage === null) return;
+    editpage.classList.add("active");
+    overlay.classList.add("active");
+  }
+
+  async function closeEditPage(e) {
+    e.preventDefault();
+
+    if (editpage === null) return;
+    editpage.classList.remove("active");
+    overlay.classList.remove("active");
+  }
+
+  async function completeEdit(id) {
+    try {
+      const response = await fetch(`http://localhost:4000/items/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      const data = await response.json();
+      console.log({ data });
+
+      closeEditPage();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  //Getting all items from inventory & storing them into state
   useEffect(() => {
     async function getItems() {
       try {
@@ -43,7 +87,6 @@ function App() {
 
         const items = await response.json();
         setItems(items);
-        console.log(items);
       } catch (err) {
         console.error(err);
       }
@@ -51,6 +94,26 @@ function App() {
 
     getItems();
   }, []);
+
+  //Deleting item
+
+  async function deleteItem(id) {
+    let answer = false;
+
+    answer = window.confirm("Are you sure you want to delete this item?");
+
+    if (answer) {
+      try {
+        const response = await fetch(`http://localhost:4000/items/${id}`, {
+          method: "DELETE",
+        });
+
+        const data = await response.json();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 
   return (
     <section>
@@ -70,15 +133,38 @@ function App() {
           <label>Items in inventory:</label>
           {items &&
             items.map((item) => {
-              const { name, id } = item;
+              const { name, location, id } = item;
 
               return (
                 <ul key={id}>
-                  <li>
-                    {name}
-                    <button>Edit</button>
-                    <button>Delete</button>
-                  </li>
+                  <li>Item: {name}</li>
+                  <li>Location: {location}</li>
+                  <button onClick={openEditPage}>Edit</button>
+                  <div className="editpage" id="editpage">
+                    <label>Name: </label>
+                    <input
+                      name="name"
+                      value={form.name}
+                      onChange={handleChange}
+                    ></input>
+                    <label>Location:</label>
+                    <input
+                      name="location"
+                      value={form.location}
+                      onChange={handleChange}
+                    ></input>
+                    <button
+                      onClick={() => completeEdit(id)}
+                      className="complete"
+                    >
+                      Complete
+                    </button>
+                    <button onClick={closeEditPage} className="cancel">
+                      Cancel
+                    </button>
+                  </div>
+                  <div id="overlay"></div>
+                  <button onClick={() => deleteItem(id)}>Delete</button>
                 </ul>
               );
             })}
